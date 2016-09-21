3c20a772388806db16ab804b654b5857b2ddaae9
Zhang Genmao <zgm4454@gmail.com>
2016-09-22 00:29:18 +0800

    修改路由设置

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory, Router, Route,	IndexRoute } from 'react-router';
+import { hashHistory, browserHistory, Router, Route,	IndexRoute } from 'react-router';
 import App from './components/Main';
 import Login from './components/Login';
 import Detail from './components/Detail';
@@ -11,13 +11,14 @@ import NotFound from './components/NotFound';
 // Render the main component into the dom
 // ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render((
-	<Router history={browserHistory} >
-		<Route path="/login" 	component={Login} />
-		<Route path="/" 		component={App}/>
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
 			<IndexRoute component={App}/>
-			<Route path="/index" 	component={App} />
-			<Route path="/detail" 	component={Detail} />
-			<Route path="/list" 	component={List} />
+			<Route path="/index" component={App}/>
+			<Route path="/detail" component={Detail}/>
+			<Route path="/list" component={List}/>
+		</Route>
+		<Route path="/login" component={Login} />
 		<Route path="*" component={NotFound} />
 	</Router>
 ), document.getElementById('app'))
